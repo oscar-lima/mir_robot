21c753fa02c67453e62455819448eb3e38af2ac2
Oscar Lima <olima_84@yahoo.com>
2025-10-10 12:30:34 +0200

    Improve PathProgressCritic intermediate goal handling

--- mir_dwb_critics/include/mir_dwb_critics/path_progress.h
+++ mir_dwb_critics/include/mir_dwb_critics/path_progress.h
@@ -63,11 +63,22 @@ protected:
                    unsigned int& y, double& desired_angle);
 
   unsigned int getGoalIndex(const std::vector<geometry_msgs::Pose2D>& plan, unsigned int start_index,
-                            unsigned int last_valid_index) const;
+                            unsigned int last_valid_index, double& desired_angle, bool& has_forward_direction) const;
+
+  bool computeOutgoingAngle(const std::vector<geometry_msgs::Pose2D>& plan, unsigned int index,
+                            double& angle) const;
+
+  bool isGoalReached(const geometry_msgs::Pose2D& robot_pose, const geometry_msgs::Pose2D& goal_pose,
+                     double goal_yaw) const;
 
   double xy_local_goal_tolerance_;
+  double yaw_local_goal_tolerance_;
   double angle_threshold_;
+  double articulation_angle_threshold_;
   double heading_scale_;
+  bool enforce_forward_dot_;
+
+  unsigned int last_progress_index_;
 
   std::vector<geometry_msgs::Pose2D> reached_intermediate_goals_;
   double desired_angle_;
